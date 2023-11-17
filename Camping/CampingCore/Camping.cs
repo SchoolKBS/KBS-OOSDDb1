e51c50b006e328a0ed30ac8d4dff8ca57029ba2f
Teunvdk <33592768+TonyChocolony@users.noreply.github.com>
2023-11-17 14:16:28 +0100

    Commit voor huidige aanpassing afsluiting vrijdag

--- Camping/CampingCore/Camping.cs
+++ Camping/CampingCore/Camping.cs
@@ -18,9 +18,10 @@ namespace CampingCore
             this.Reservations = new ObservableCollection<Reservation>();
             for (int i = 1; i <= 10; i++)
             {
-                Places.Add(new Place(i, true, 1, 1, 1));
+                Places.Add(new Place(i, true, 1, 1, i));
                 Reservations.Add(new Reservation(i, DateTime.Now.AddDays(i), DateTime.Now.AddDays(i + 10), Places[i - 1]));
             }
+
 
         }
     }
