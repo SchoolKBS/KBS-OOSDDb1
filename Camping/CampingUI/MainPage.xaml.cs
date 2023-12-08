dde0b83768b26fa2faeda7fdd3fd9bb0c28d5b4f
Jens Bouma <jensmbouma@kpnmail.nl>
2023-12-08 15:03:55 +0100

    Straat zichtbaar on hover

--- Camping/CampingUI/MainPage.xaml.cs
+++ Camping/CampingUI/MainPage.xaml.cs
@@ -185,7 +185,6 @@ namespace CampingUI
                 {
                     rotate = new RotateTransform(90, 0, 0);
                 }
-
                 Grid canvasStreet = new Grid
                 {
                     Width = coordinates[2],
@@ -198,7 +197,7 @@ namespace CampingUI
 
                 TextBlock textBlock = new TextBlock
                 {
-                    Text = street.Name,
+                    Text = "",
                     Foreground = Brushes.White,
                     LayoutTransform = rotate,
                     HorizontalAlignment = HorizontalAlignment.Center,
@@ -206,19 +205,28 @@ namespace CampingUI
                     FontSize = 10,
                 };
 
+                // Set the TextBlock as the Tag of the Grid
+                canvasStreet.Tag = textBlock;
+
                 canvasStreet.Children.Add(textBlock);
 
                 canvasStreet.MouseEnter += (sender, e) =>
                 {
-                    canvasStreet.Background = Brushes.Gray; // Change the background color on hover
+                    canvasStreet.Background = Brushes.DarkCyan; // Change the background color on hover
+                    TextBlock streetName = (TextBlock)((Grid)sender).Tag;
+                    streetName.Text = street.Name;
+
+                    Canvas.SetZIndex(canvasStreet, Canvas.GetZIndex(canvasStreet) + 1);
                 };
 
                 canvasStreet.MouseLeave += (sender, e) =>
                 {
                     canvasStreet.Background = Brushes.Black;
+                    TextBlock streetName = (TextBlock)((Grid)sender).Tag;
+                    streetName.Text = "";
+
+                    Canvas.SetZIndex(canvasStreet, Canvas.GetZIndex(canvasStreet) - 1);
                 };
-
-
 
                 Canvas.SetTop(canvasStreet, coordinates[1]);  // Ycord1 to place from top.
                 Canvas.SetLeft(canvasStreet, coordinates[0]); // XCord1 to place from left.
