b56671528d5a480679d2f31735a4af6a5b76541c
Teunvdk <33592768+TonyChocolony@users.noreply.github.com>
2024-01-10 12:19:09 +0100

    commit comment AAA mock
    arrange, act en assert comments toegevoegd aan de unit testen

--- Camping/UnitTests/PlacesOverviewPageFilterTests.cs
+++ Camping/UnitTests/PlacesOverviewPageFilterTests.cs
@@ -12,45 +12,69 @@ namespace UnitTests
         [Test]
         public void GetFilteredListOnPower_places_returnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Place> places = new List<Place>();
+            
+            //Act
             places = PlacesOverviewFilter.GetFilteredListOnPower(true, camping.GetPlaces(), camping);
+            
+            //Assert
             Assert.That(places.Count(), Is.EqualTo(5));
         }
 
         [Test]
         public void GetFilteredListOnAmountOfPeople_places_returnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Place> places = new List<Place>();
+            
+            //Act
             places = PlacesOverviewFilter.GetFilteredListOnAmountOfPeople(6, camping.GetPlaces(), camping);
+            
+            //Assert
             Assert.That(places.Count(), Is.EqualTo(5));
         }
         [Test]
         public void GetFilteredListOnPrice_places_returnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Place> places = new List<Place>();
+            
+            //Act
             places = PlacesOverviewFilter.GetFilteredListOnPrice(5.5, camping.GetPlaces(), camping);
+            
+            //Assert
             Assert.That(places.Count(), Is.EqualTo(5));
         }
 
         [Test]
         public void GetFilteredListOnDates_places_returnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Place> places = new List<Place>();
+            
+            //Act
             places = PlacesOverviewFilter.GetFilteredListOnDate(false, DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(5), camping.GetPlaces(), camping);
+            
+            //Assert
             Assert.That(places.Count(), Is.EqualTo(5));
         }
 
         [Test]
         public void GetAvailablePlacesBetweenDates_places_returnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
-
             IEnumerable<Place> places = new List<Place>();
+
+            //Act
             places = PlacesOverviewFilter.GetAvailablePlacesBetweenDates(DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(5), camping);
+            
+            //Assert
             Assert.That(places.Count(), Is.EqualTo(5));
         }
     }
--- Camping/UnitTests/ReservationsOverviewFilterTests.cs
+++ Camping/UnitTests/ReservationsOverviewFilterTests.cs
@@ -16,27 +16,42 @@ namespace UnitTests
         [Test]
         public void GetFilteredListOn_IsPaid_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.IsPaid == true);
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(5));
         }
 
         [Test]
         public void GetFilteredListOn_IsNotPaid_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.IsPaid == false);
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(5));
         }
         [Test]
 
         public void GetFilteredListOn_ReservationNumber_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.ReservationID == 1);
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(1));
         }
 
@@ -44,39 +59,59 @@ namespace UnitTests
 
         public void GetFilteredListOn_PlaceNumber_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.PlaceID == 1);
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(1));
         }
         [Test]
 
         public void GetFilteredListOn_ArrivalDate_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.ArrivalDate >= DateTime.Now.AddDays(2));
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(8));
         }
         [Test]
 
         public void GetFilteredListOn_LeaveDate_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where(reservation => reservation.DepartureDate <= DateTime.Now.AddDays(3));
+            
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(2));
         }
         [Test]
 
         public void GetFilteredListOn_GuestNameFull_ReturnsList()
         {
+            //Arrange
             Camping camping = new Camping(TestSupportClass.MockIcampingRepository().Object);
             IEnumerable<Reservation> reservations = new List<Reservation>();
+            
+            //Act
             reservations = camping.CampingRepository.CampingReservationRepository.GetReservations().Where
     (reservation =>
           reservation.GuestID.Equals(6)
       ); 
+
+            //Assert
             Assert.That(reservations.Count(), Is.EqualTo(1));
         }
     }
